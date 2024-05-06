f368a5f08965f2891271d85d680247260f715e84
MrBl4ck <franz.carvajal04@gmail.com>
2024-05-06 00:29:11 -0400

    Código base del modal de agendar citas

--- src/modules/core/script.js
+++ src/modules/core/script.js
@@ -17,6 +17,26 @@ const addEventOnElem = function (elem, type, callback) {
 /**
  * navbar toggle
  */
+document.getElementById('navReserveLink').addEventListener('click', function(event) {
+  event.preventDefault();  // Esto evita que la página se desplace hacia arriba cuando hagas clic en el enlace
+  document.getElementById('reserveModal').style.display = 'flex';
+});
+
+document.getElementById('openReserveModal').addEventListener('click', function() {
+  document.getElementById('reserveModal').style.display = 'block';
+});
+
+document.querySelector('.modal .close').addEventListener('click', function() {
+  this.closest('.modal').style.display = 'none';
+});
+
+document.getElementById('reserveForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Aquí podrías agregar el código para enviar los datos al servidor
+  console.log('Reserva hecha');
+  this.closest('.modal').style.display = 'none';
+});
+
 
 const navToggler = document.querySelector("[data-nav-toggler]");
 const navbar = document.querySelector("[data-navbar]");
