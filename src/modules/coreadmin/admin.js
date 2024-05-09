a121539f58f91b30270c9b7b42c51607508e9de3
luwu <lucivp22@gmail.com>
2024-05-08 22:58:10 -0400

    Ajustes Barra de Navegación

--- src/modules/coreadmin/admin.js
+++ src/modules/coreadmin/admin.js
@@ -32,3 +32,27 @@ menuBtn.addEventListener('click', () => {
     }
     isMenuOpen = !isMenuOpen;
 });
+
+function toggleDropdown() {
+    var dropdown = document.getElementById('profileDropdown');
+    // Alternar la visibilidad del menú desplegable
+    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+}
+
+// Aseguramos que el manejador de eventos se ejecute después de que la página haya cargado completamente
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdowns = document.querySelectorAll('.profile-dropdown');
+
+    // Función para cerrar el menú si se hace clic fuera de él
+    window.onclick = function(event) {
+        // Verificar si el clic fue fuera de cualquier área del perfil y del menú desplegable
+        if (!event.target.closest('.profile') && !event.target.closest('.profile-dropdown')) {
+            // Iterar sobre cada menú desplegable para ocultarlo si está abierto
+            dropdowns.forEach(function(dropdown) {
+                if (dropdown.style.display === 'block') {
+                    dropdown.style.display = 'none';
+                }
+            });
+        }
+    };
+});
