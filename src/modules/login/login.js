13c7dbae8a7b6962e770acba45cb63525de9569e
BleeeesJc <matevalen.xd@gmail.com>
2024-05-08 20:59:10 -0400

    Rama Catalogo + Rama ConexionBD

---
+++ src/modules/login/login.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const botonIngresar = document.getElementById('ingresarBtn');
+    if (botonIngresar) {
+        botonIngresar.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevenir el comportamiento por defecto del botón de submit
+            iniciarSesion();
+        });
+    } else {
+        console.error('El botón de ingreso no se encontró en el DOM');
+    }
+});
+
+function iniciarSesion() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    fetch('http://localhost/GoCan/src/modules/php/login.php', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.estado === "success") {
+            // Redirigir al usuario basado en el valor de cargo
+            if (data.cargo) {
+                window.location.href = 'http://localhost/GoCan/src/modules/core/index.html';
+            } else {
+                window.location.href = 'http://localhost/GoCan/src/modules/coreadmin/indexadmin.html';
+            }
+        } else {
+            alert(data.mensaje); // Mostrar mensaje de error
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error al procesar la solicitud');
+    });
+}
---
+++ src/modules/php/registro.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-Type: application/json');
+
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
+$email = $data->email;
+$nombre = $data->nombre;
+$password = $data->password;
+$token = null; // Definido como null porque es gestionado internamente
+
+$conexion = pg_connect("dbname=gocan user=postgres password=admin");
+if (!$conexion) {
+    echo json_encode(["estado" => "error_conexion"]);
+    exit();
+}
+
+// Asegúrate de que los identificadores sean únicos o simplemente no los uses.
+$sql_usuario = "INSERT INTO usuario (email, nombre, password, token) VALUES ($1, $2, $3, $4) RETURNING id_usuario";
+$stmt = pg_prepare($conexion, "insert_usuario", $sql_usuario);
+
+if ($stmt === false) {
+    echo json_encode(["estado" => "error_preparar_consulta"]);
+    exit();
+}
+
+$resultado_usuario = pg_execute($conexion, "insert_usuario", array($email, $nombre, $password, $token));
+if (!$resultado_usuario) {
+    echo json_encode(["estado" => "error_insertar_usuario"]);
+    exit();
+}
+
+$id_usuario = pg_fetch_result($resultado_usuario, 0, 'id_usuario');
+echo json_encode(["estado" => "usuario_registrado", "id_usuario" => $id_usuario]);
+
+pg_close($conexion);
+?>
