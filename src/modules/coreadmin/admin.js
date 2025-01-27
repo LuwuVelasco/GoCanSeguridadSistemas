347ef43680cb2f4fd1c7c4ad2b5bb8cd85acb56a
JoshNisth <134982802+JoshNisth@users.noreply.github.com>
2025-01-27 18:02:04 -0400

    Add log de usuario al cambiar configuarcion

--- src/modules/coreadmin/admin.js
+++ src/modules/coreadmin/admin.js
@@ -5,18 +5,22 @@ import { loadActivities } from '../components/actividades.js';
 import { eliminarDoctor, loadData, initFuncionarioForm } from '../components/funcionarios.js';
 import { loadDoctorReports } from '../components/reportes.js';
 import { loadEspecialidades, loadRolesFuncionario } from '../components/loadSelects.js';
+import { initConfigPasswordForm } from '../components/configuracion_password.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Configuración de modales
+    // 1) Configuración de modales
     setupModalCloseOnOutsideClick();
 
-    // Cargar calificaciones
+    // 2) Inicializar el formulario de configuración de contraseñas
+    initConfigPasswordForm();
+
+    // 3) Cargar calificaciones
     loadRatings();
 
-    // Cargar la tabla de mascotas
+    // 4) Cargar la tabla de mascotas
     loadMascotasTable('http://localhost/GoCanSeguridadSistemas/src/modules/php/obtener_mascotas.php', '#lista-mascotas');   
 
-    // Cargar los reportes en el modal
+    // 5) Cargar los reportes en el modal
     loadDoctorReports('http://localhost/GoCanSeguridadSistemas/src/modules/php/obtener_reporte.php', '#reportHistory');
 
     document.getElementById('historySearchInput').addEventListener('input', function () {
@@ -29,13 +33,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
     
-    // Configurar el formulario para registrar doctores
+    // 6) Configurar el formulario para registrar doctores
     initFuncionarioForm(
         "#registroFuncionario",
         "http://localhost/GoCanSeguridadSistemas/src/modules/php/cargar_especialidades.php"
     );
 
-    // Cargar actividades y doctores
+    // 7) Cargar actividades y doctores
     loadActivities('http://localhost/GoCanSeguridadSistemas/src/modules/php/get_actividades.php', '#actividades-table tbody');
     loadData('http://localhost/GoCanSeguridadSistemas/src/modules/php/listadoctores.php', '#lista-veterinarios');
     
@@ -44,11 +48,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     loadRolesFuncionario('http://localhost/GoCanSeguridadSistemas/src/modules/php/obtener_roles.php', '#rol');
 
-    // Configuración de botones
+    // 8) Configuración de botones
     document.getElementById('bt1').addEventListener('click', () => openModal('historyModal'));
     document.getElementById('bt2').addEventListener('click', () => openModal('reserveModal'));
     document.getElementById('bt3').addEventListener('click', () => openModal('tablaModal'));
 
+    // Hacer disponibles las funciones de abrir/cerrar modal en el ámbito global
     window.openModal = openModal;
     window.closeModal = closeModal;
 });
