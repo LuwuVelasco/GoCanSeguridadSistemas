789f06a85d2cda1048d10b9458e95a531b93a3df
Luciana Velasco <luwuvp22@gmail.com>
2025-12-02 21:04:17 -0400

    Tiempo espera test aceptación Lu

--- gocan-aceptacion/src/test/java/CrearFuncionarioTest.java
+++ gocan-aceptacion/src/test/java/CrearFuncionarioTest.java
@@ -42,6 +42,7 @@ public class CrearFuncionarioTest {
         // ===============================================
         String loginUrl = "http://localhost/GoCanSeguridadSistemas/src/modules/core/";
         driver.get(loginUrl);
+        esperar(2);
 
         // ===============================================
         // FASE 2: LÓGICA DE PRUEBA
